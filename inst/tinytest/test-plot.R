0d7ca0f18f6211c83095db50923cf5cd9481ebe6
Marc Bosch Matas <mboschmatas@gmail.com>
2024-05-13 11:43:12 +0000

    version 0.0.2

---
+++ inst/tinytest/test-plot.R
@@ -0,0 +1,24 @@
+options(tinysnapshot_os = "Windows") # see Sys.info()["sysname"]
+options(tinysnapshot_device = "svglite")
+options(tinysnapshot_device_args = list(user_fonts = fontquiver::font_families("Liberation")))
+
+
+using("tinysnapshot")
+library(ggplot2)
+
+
+p <- ggplot(airquality, aes(Day, Wind, group = Month)) + 
+  stat_interquartilerange(geom = "ribbon",
+                          show.legend = FALSE) +
+  geom_line() + 
+  stat_sparklabels(geom = "label", label_fun = \(x) round(x, 0),
+                   show.legend = FALSE) +
+  scale_y_continuous(limits = c(0, 25)) + 
+  facet_grid(Month~.) +
+  ggtitle("Daily wind intensity by month in NYC") + 
+  theme_minimal()
+
+
+expect_snapshot_plot(p, label = "ggplot2_example")
+
+
